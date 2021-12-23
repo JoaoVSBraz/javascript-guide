4acaecec0d2ec1cf164fafd61ad469160e8a2531
JoaoVSBraz <joaovictor3.14@outlook.com>
2021-12-23 15:40:39 -0300

    :construction: Add ex008

---
+++ ex008.js
@@ -0,0 +1,9 @@
+var velocidadeCarro = 61
+
+console.log(`Sua velocidade é de ${velocidadeCarro}Km/h`)
+
+if (velocidadeCarro > 60) {
+    console.log('Você ultrapassou o limite de velocidade e foi multado por isso.')
+}
+
+console.log('Dirija com cuidade sempre!')
