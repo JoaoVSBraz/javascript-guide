96e3843aebee77cce853dc00061a3e83435f2f48
JoaoVSBraz <joaovictor3.14@outlook.com>
2021-12-28 10:29:22 -0300

    :recycle: Update logic  condition at ex018 script.js

--- exercicios/ex018/script.js
+++ exercicios/ex018/script.js
@@ -3,20 +3,19 @@ function contagem() {
     var inputFim = window.document.getElementById('fim')
     var inputPasso = window.document.getElementById('passo')
     var resultado = window.document.getElementById('resultado')
-    var content = window.document.createElement('p')
 
     var inicio = Number(inputInicio.value)
     var fim = Number(inputFim.value)
     var passo = Number(inputPasso.value)
 
-    if (passo == 0 || passo == null || passo == undefined) {
-        alert('[ERRO] O passo deve ser maior que 0.')
+    if (inputInicio.value.length == 0 || inputFim.value.length == 0 || inputPasso.value.length == 0) {
+        alert('[ERRO] Digite um número')
     } else if (inicio < fim) {
-        for( inicio ; inicio < fim ; inicio += passo ) {
+        for( inicio ; inicio <= fim ; inicio += passo ) {
             resultado.innerHTML += ` ${inicio} `
         }
     } else {
-        for( inicio ; inicio > fim ; inicio -= passo ) {
+        for( inicio ; inicio >= fim ; inicio -= passo ) {
             resultado.innerHTML += ` ${inicio} `
         }
     }
