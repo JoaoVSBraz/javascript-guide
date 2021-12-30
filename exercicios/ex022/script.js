e5d305af0ae877c094cf4b90fcc4feb6e5810ead
JoaoVSBraz <joaovictor3.14@outlook.com>
2021-12-30 11:01:56 -0300

    :construction: Update ex022

--- exercicios/ex022/script.js
+++ exercicios/ex022/script.js
@@ -1,8 +1,10 @@
 var conjunto = []
+let numero = window.document.getElementById('numero')
+let arrayNum = window.document.getElementById('array')
+let resultado = window.document.getElementById('resultado')
 
 function executar() {
-    let numero = window.document.getElementById('numero')
-    let arrayNum = window.document.getElementById('array')
+
     let num = Number(numero.value)
 
     if (numero.value.length == 0) {
@@ -10,29 +12,38 @@ function executar() {
     } else {
         conjunto.push(num)
         arrayNum.innerHTML += `<p><strong>${num}</strong> adicionado</p>`
+        resultado.innerHTML = ''
     }
 }
 
 function calc() {
-    let resultado = window.document.getElementById('resultado')
     let soma = 0
+    let media = 0
     let maior, menor
 
-    for(var i=0; i < conjunto.length; i++) {
-        soma += conjunto[i]
-        
-        if (i == 0) {
-            maior = conjunto[i]
-            menor = conjunto[i]
+    if (conjunto.length == 0) {
+        alert('ERRO: Adicione valores antes de finalizar')
+    } else {
+        for(var i=0; i < conjunto.length; i++) {
+            soma += conjunto[i]
+            
+            if (i == 0) {
+                maior = conjunto[i]
+                menor = conjunto[i]
+            }
+            if (conjunto[i] > maior) {
+                maior = conjunto[i]
+            } else if (conjunto[i] < menor) {
+                menor = conjunto[i]
+            }
         }
-        if (conjunto[i] > maior) {
-            maior = conjunto[i]
-        } else if (conjunto[i] < menor) {
-            menor = conjunto[i]
-        }
+        media = soma / conjunto.length
+        resultado.innerHTML += `<p>A quantidade de números são ${conjunto.length}</p>`
+        resultado.innerHTML += `<p>A soma dos valores é ${soma}</p>`
+        resultado.innerHTML += `<p>O maior número foi ${maior}</p>`
+        resultado.innerHTML += `<p>O menor número foi ${menor}</p>`
+        resultado.innerHTML += `<p>A média é igual a ${media}</p>`
     }
-    resultado.innerHTML += `<p>A soma dos valores do array é ${soma}</p>`
-    resultado.innerHTML += `<p>O maior número foi ${maior}</p>`
-    resultado.innerHTML += `<p>O menor número foi ${menor}</p>`
-
 }
+
+// RESOLUÇÃO POR JOÃO BRAZ
