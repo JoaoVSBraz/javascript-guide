a653cfc72e30a6a73860ec46a901d1c90c9afbbf
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-01-04 10:47:27 -0300

    :construction: Add ex026

---
+++ exercicios/ex026.js
@@ -0,0 +1,16 @@
+function getMilk(money) {
+    //bottle of milk == 1.5
+    bottlesOfMilk = parseInt(money / 1.5) 
+
+    console.log('move')
+    console.log('move')
+    console.log('turn left')
+    console.log('move')
+    console.log('move')
+    console.log('turn right')
+    console.log('move')
+    console.log('move')
+    console.log('buy ' + bottlesOfMilk + ' bottles of milk')
+}
+
+getMilk(5)
