806a1f8801b54672e3132affd64ae6c3c07e7288
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-01-04 15:49:48 -0300

    :construction: Update ex028

--- exercicios/ex028.js
+++ exercicios/ex028.js
@@ -1,7 +1,15 @@
 function bmiCalculator(weight, height) {
-    let bmi = weight / Math.pow(height, 2)
-    return Math.round(bmi)
+    let bmi = weight / Math.pow(height, 2);
+    let interpretation = '';
+    if(bmi < 18.5) {
+        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+    } else if(bmi < 24.9) {
+        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+    } else {
+        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+   
+    }
+    return interpretation;
 }
 
-let john = bmiCalculator(65, 1.8)
-console.log(john)
+console.log(bmiCalculator(60, 2))
