812874e8eacdd6585af59cfb616f266eb6ac1f2b
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-01-06 15:00:08 -0300

    :construction: Add ex036

---
+++ exercicios/ex036/script.js
@@ -0,0 +1,10 @@
+let huge = document.getElementById('title')
+
+// adicionando uma classe através do método add()
+huge.classList.add('huge')
+
+// removendo uma classe através do método remove()
+huge.classList.remove('huge')
+
+// adicionando uma classse através do método toggle()
+huge.classList.toggle('huge')
