307ec319c77c4b609408a65f852e7539d116ba7c
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-01-06 14:58:07 -0300

    :construction: Update ex025 script.js

--- exercicios/ex025/script.js
+++ exercicios/ex025/script.js
@@ -1,7 +1,6 @@
 function AgeCalculator() {
     let dogAge = prompt("What is your dog's age?")
     let humanAge = ((Number(dogAge) - 2 ) * 4) + 21
-    let resultado = document.getElementById('resultado')
 
     alert("Your dog's human age is " + humanAge + " years old")
 }
