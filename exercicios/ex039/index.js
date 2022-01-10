cf0090ede7f8815974b743d12c5b98a981907626
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-01-10 13:24:50 -0300

    :construction: Update ex039  with click and keypress events

--- exercicios/ex039/index.js
+++ exercicios/ex039/index.js
@@ -16,45 +16,56 @@ let numberOfDrumButtons = document.querySelectorAll('.drum').length
 
 for(let i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll('.drum')[i].addEventListener('click', function() {
-        switch (this.textContent) {
-            case 'w':
-                let tom1 = new Audio('sounds/tom-1.mp3')
-                tom1.play()
-                break
-
-            case 'a':
-                let tom2 = new Audio('sounds/tom-2.mp3')
-                tom2.play()
-                break
-
-            case 's':
-                let tom3 = new Audio('sounds/tom-3.mp3')
-                tom3.play()
-                break
-
-            case 'd':
-                let tom4 = new Audio('sounds/tom-4.mp3')
-                tom4.play()
-                break
-
-            case 'j':
-                let snare = new Audio('sounds/snare.mp3')
-                snare.play()
-                break
-            
-            case 'k':
-                let kick = new Audio('sounds/kick-bass.mp3')
-                kick.play()
-                break
-
-            case 'l':
-                let crash = new Audio('sounds/crash.mp3')
-                crash.play()
-                break
-        
-            default:
-                alert('Erro: Try again')
-                break
-        }
+        var buttonTextoContent = this.textContent
+        makeSound(buttonTextoContent)
     })
+}
+
+document.addEventListener('keydown', function(event) {
+
+    makeSound(event.key)
+
+})
+
+function makeSound(key) {
+    switch (key) {
+        case 'w':
+            let tom1 = new Audio('sounds/tom-1.mp3')
+            tom1.play()
+            break
+
+        case 'a':
+            let tom2 = new Audio('sounds/tom-2.mp3')
+            tom2.play()
+            break
+
+        case 's':
+            let tom3 = new Audio('sounds/tom-3.mp3')
+            tom3.play()
+            break
+
+        case 'd':
+            let tom4 = new Audio('sounds/tom-4.mp3')
+            tom4.play()
+            break
+
+        case 'j':
+            let snare = new Audio('sounds/snare.mp3')
+            snare.play()
+            break
+        
+        case 'k':
+            let kick = new Audio('sounds/kick-bass.mp3')
+            kick.play()
+            break
+
+        case 'l':
+            let crash = new Audio('sounds/crash.mp3')
+            crash.play()
+            break
+    
+        default:
+            alert('Erro: Try again')
+            break
+    }
 }
